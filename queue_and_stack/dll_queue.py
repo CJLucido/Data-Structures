c3dad01d28c9c05f683b10a68bb3b42bae27fda3
CJ Lucido <c.jose.lucido@gmail.com>
2020-03-23 12:54:46 -0700

    tested queue and seems to get through all tests except dequeue, which is good because I haven't built that yet

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -5,15 +5,30 @@ from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
-        self.size = 0
+        # should increase when we enqueue and decrease if we dequeue OR should always just match the dll length
+        self.size = 0 
         # Why is our DLL a good choice to store our elements?
+        # Because we don't have to worry about whether we have another spot in memory at the tail end (and then pop the previous head) or shift everything over an index if we delete from the head to add to the tail.
+        # This reduces our runtime complexity from O(n) in the latter case to O(1).
+
         # self.storage = ?
+        # SCRATCH THIS we need somewhere to store all the node values, and then with the values we can change the head and the tail using the doubly_linked_list class
+        # we don't need to store all the node values here because if we have the head we can get to the whole list, we only care about changing the head and tail, nothing in the middle
+
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
+        #replace the head with the new value
+        self.storage.add_to_head(value)
+        # then we update our size here to match the new dll length given by this addition
+        self.size = self.storage.length
         pass
 
     def dequeue(self):
+        #remove the tail and replace it with the previous value
         pass
 
     def len(self):
-        pass
+        #return the self.size after fetching the dll length or just return the self.size if it has been updating with the queue methods
+        self.size = self.storage.length
+        return self.size
