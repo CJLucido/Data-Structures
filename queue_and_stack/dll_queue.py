940d2e39cd3271137bc647ef12b6dde224e7b3c4
CJ Lucido <c.jose.lucido@gmail.com>
2020-03-23 13:39:29 -0700

    needed to handle the situation if the tail was none in the dequeue, all tests passing

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -15,18 +15,27 @@ class Queue:
         # SCRATCH THIS we need somewhere to store all the node values, and then with the values we can change the head and the tail using the doubly_linked_list class
         # we don't need to store all the node values here because if we have the head we can get to the whole list, we only care about changing the head and tail, nothing in the middle
 
-        self.storage = DoublyLinkedList()
+        self.storage = DoublyLinkedList() #should be the whole class because the class will have both a head and a tail on it
 
     def enqueue(self, value):
         #replace the head with the new value
         self.storage.add_to_head(value)
         # then we update our size here to match the new dll length given by this addition
         self.size = self.storage.length
-        pass
+        # print(self.storage.tail.value)
+        
 
     def dequeue(self):
         #remove the tail and replace it with the previous value
-        pass
+        if self.storage.tail is None:
+            removed_from_queue = None
+        else:
+            removed_from_queue = self.storage.tail.value
+            self.storage.remove_from_tail()
+        
+        # then we update our size here to match the new dll length given by this subtraction
+        self.size = self.storage.length
+        return removed_from_queue
 
     def len(self):
         #return the self.size after fetching the dll length or just return the self.size if it has been updating with the queue methods
